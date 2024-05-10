a5c24308bf2c1554eebfc7c430932682e06ae8d9
Enrico Spagnoli <Enrico Spagnoli@LAPTOP-ENRICO>
2024-05-10 09:05:56 +0200

    Aggiungere i file di progetto.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace AS2324._3G.SpagnoliEnrico.PSDBPCTO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
